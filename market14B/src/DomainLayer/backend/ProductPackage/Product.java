b42da95b7d9a1d6feb28aefeff7b199be41b2070
MagedNatCs <magednat@post.bgu.ac.il>
2024-05-27 13:51:30 +0300

    first Product Controller impl

--- market14B/src/DomainLayer/backend/ProductPackage/Product.java
+++ market14B/src/DomainLayer/backend/ProductPackage/Product.java
@@ -68,4 +68,14 @@ public class Product {
         this.rating = rating;
     }
 
+    public String getInfo(){
+        StringBuilder info = new StringBuilder();
+        info.append("Product Information:\n");
+        info.append("Name: ").append(name).append("\n");
+        info.append("Brand: ").append(brand).append("\n");
+        info.append("Category: ").append(category.getName()).append("\n");
+        info.append("Rating: ").append(rating);
+        info.append("Description: ").append(description).append("\n");
+        return info.toString();
+    }
 }
