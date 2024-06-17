76473581960b7432593b9dec5a4f4871bd886e20
Waleed Abu Kaf <waleedab@post.bgu.ac.il>
2024-06-17 13:51:51 +0300

    changed deprecated methods in TokenService.

--- market14B/src/main/java/ServiceLayer/TokenService.java
+++ market14B/src/main/java/ServiceLayer/TokenService.java
@@ -1,54 +1,58 @@
-// package ServiceLayer;
+ package main.java.ServiceLayer;
 
-// import io.jsonwebtoken.Claims;
-// import io.jsonwebtoken.Jwt;
-// import io.jsonwebtoken.Jwts;
-// import org.springframework.beans.factory.annotation.Value;
+ import io.jsonwebtoken.Claims;
+ import io.jsonwebtoken.Jwts;
+ import org.springframework.beans.factory.annotation.Value;
 
-// import javax.crypto.SecretKey;
-// import java.util.Date;
-// import java.util.function.Function;
+ import javax.crypto.SecretKey;
+ import java.util.Date;
+ import java.util.function.Function;
 
-// public class TokenService {
+ public class TokenService {
 
-//     @Value("${jwt.secret}")
-//     private String secret;
+     @Value("${jwt.secret}")
+     private String secret;
 
-//     private final long expirationTime = 1000*60*60*24;
-//     private SecretKey key = Jwts.SIG.HS256.key().build();
+     private final long expirationTime = 1000*60*60*24;
+     private SecretKey key = Jwts.SIG.HS256.key().build();
 
-//     public String generateToken(String username){
-//         return Jwts.builder()
-//                 .setSubject(username)
-//                 .setIssuedAt(new Date(System.currentTimeMillis()))
-//                 .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
-//                 .signWith(key)
-//                 .compact();
-//     }
+     public String generateToken(String username){
+         return Jwts.builder()
+                 .subject(username)
+                 .issuedAt(new Date(System.currentTimeMillis()))
+                 .expiration(new Date(System.currentTimeMillis() + expirationTime))
+                 .signWith(key)
+                 .compact();
+     }
 
-//     public boolean validateToken(String token){
-//         try {
-//             Jwts.parser().setSigningKey(key).build().parseClaimsJws(token);
-//             return true;
-//         }catch (Exception e){
-//             return false;
-//         }
-//     }
+     public boolean validateToken(String token){
+         try {
+             Jwts.parser().verifyWith(key).build().parseSignedClaims(token);
+             return true;
+         }catch (Exception e){
+             return false;
+         }
+     }
 
-//     public String extractUsername(String token){
-//         return extractClaim(token, Claims::getSubject);
-//     }
+     public String extractUsername(String token){
+         return extractClaim(token, Claims::getSubject);
+     }
 
-//     public Date extractExpiration(String token){
-//         return extractClaim(token, Claims::getExpiration);
-//     }
+     public Date extractExpiration(String token){
+         return extractClaim(token, Claims::getExpiration);
+     }
 
-//     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
-//         final Claims claims = extractAllClaims(token);
-//         return claimsResolver.apply(claims);
-//     }
-//     private Claims extractAllClaims(String token){
-//         return Jwts.parser().setSigningKey(key).build().parseClaimsJws(token).getBody();
-//     }
+     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+         final Claims claims = extractAllClaims(token);
+         return claimsResolver.apply(claims);
+     }
+     private Claims extractAllClaims(String token){
+             return Jwts
+                     .parser()
+                     .verifyWith(key)
+                     .build()
+                     .parseSignedClaims(token)
+                     .getPayload();
+     }
 
-// }
+ }
